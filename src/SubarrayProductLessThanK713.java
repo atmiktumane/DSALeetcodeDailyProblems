f7ede7453f0723d7c0bffbebabecb2795fb53c0b
atmiktumane <atmiktumane72@gmail.com>
2024-03-27 20:55:53 +0530

    leetcode question 713 optimal solution

--- src/SubarrayProductLessThanK713.java
+++ src/SubarrayProductLessThanK713.java
@@ -17,6 +17,37 @@ public class SubarrayProductLessThanK713 {
         return count;
     }
 
+    static int numSubarrayProductLessThanKOptimal(int[] nums, int k) {
+        int n = nums.length;
+
+        // edge case
+        if(k<=1 || n==0){ // k=0  or  k=1
+            return 0; // 0 total subarrays with product less than 1 because of constraint i.e., 1 <= nums[i] <= 1000
+        }
+
+        int i=0, j=0;
+
+        int res = 1;
+        int count = 0;
+
+        while(j < n){
+            res *= nums[j];
+
+            while(res>=k && i<=j){
+                res /= nums[i];
+                i++;
+            }
+
+            if(res < k){
+                count += j-i+1;
+
+            }
+            j++;
+        }
+        return count;
+    }
+
+
     public static void main(String[] args){
         int[] arr = {10,5,2,6};
         int k = 100;
@@ -24,6 +55,10 @@ public class SubarrayProductLessThanK713 {
         // Brute Force : Nested For Loop  ->  TC: O(N^2) , SC: O(1)
         int ans1 = numSubarrayProductLessThanKBrute(arr, k);
         System.out.println("Count total no. of subarrays whose product is strictly less than k : "+ans1);
+
+        // Optimal Approach : Sliding Window  ->  TC: O(N) , SC: O(1)
+        int ans = numSubarrayProductLessThanKOptimal(arr, k);
+        System.out.println("Count total no. of subarrays whose product is strictly less than k : "+ans);
 
     }
 }
