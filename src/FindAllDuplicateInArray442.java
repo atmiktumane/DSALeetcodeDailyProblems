5f85a83971521a2cdde8751f90b83f8600e637a4
atmiktumane <atmiktumane72@gmail.com>
2024-04-01 09:42:32 +0530

    added optimal solution for leetcode question 442

--- src/FindAllDuplicateInArray442.java
+++ src/FindAllDuplicateInArray442.java
@@ -1,6 +1,6 @@
 // Question : 442. Find All Duplicates in an Array
 // TC : O(N)
-// SC : O(N) -> HashMap space
+// SC : O(1)
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -23,7 +23,7 @@ public class FindAllDuplicateInArray442 {
         return res;
     }
 
-    static List<Integer> findDuplicatesOptimal(int[] arr) {
+    static List<Integer> findDuplicatesBetter(int[] arr) {
         int n = arr.length;
         List<Integer> res = new ArrayList<>();
 
@@ -49,20 +49,43 @@ public class FindAllDuplicateInArray442 {
         return res;
     }
 
+    static List<Integer> findDuplicatesOptimal(int[] nums){
+        int n = nums.length;
+
+        List<Integer> res = new ArrayList<>();
+
+        if(n==0 || n==1) return res;
+
+        for(int i=0; i<n; i++){
+            int num = Math.abs(nums[i]);
+            int idx = num-1;
+
+            if(nums[idx] <0) res.add(num);
+            else{
+                nums[idx] *= -1;
+            }
+        }
+        return res;
+    }
+
+
     public static void main(String[] args){
         int[] arr = {4,3,2,7,8,2,3,1};
 
         List<Integer> ans1 = new ArrayList<>();
-        List<Integer> ans = new ArrayList<>();
+        List<Integer> ans2 = new ArrayList<>();
+        List<Integer> ans3 = new ArrayList<>();
 
         // Brute Force Approach -> Nested For Loop   TC : O(N^2)
         ans1 = findDuplicatesBrute(arr);
         System.out.println("All Duplicate elements present in array : " + ans1);
 
-        // Optimal Approach  -> HashMap
-        ans = findDuplicatesOptimal(arr);
-        System.out.println("All Duplicate elements present in array : " + ans);
+        // Better Approach  -> HashMap      -> TC: O(N)  ;  SC: O(N)
+        ans2 = findDuplicatesBetter(arr);
+        System.out.println("All Duplicate elements present in array : " + ans2);
 
-
+        // Optimal Approach  -> elements in range [1,n], mark visited elements to -ve
+        ans3 = findDuplicatesOptimal(arr); // ->  TC: O(N)  ;  SC: O(1)
+        System.out.println("All Duplicate elements present in array : " + ans3);
     }
 }
