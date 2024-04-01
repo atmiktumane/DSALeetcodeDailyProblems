14bde86af1f0b983d4b7809b0fdd3792eb6da530
atmiktumane <atmiktumane72@gmail.com>
2024-04-01 09:34:24 +0530

    added optimal solution for question 287

--- src/FindDuplicateNumber287.java
+++ src/FindDuplicateNumber287.java
@@ -1,8 +1,12 @@
+// Question : 287. Find the duplicate element
+// TC : O(N)
+// SC : O(1)
+
 import java.util.HashMap;
 
 public class FindDuplicateNumber287 {
 
-    static int findDuplicate(int[] arr) {
+    static int findDuplicateBetter(int[] arr) {
         int n = arr.length;
         if(n==0 || n==1) return -1;
 
@@ -28,11 +32,31 @@ public class FindDuplicateNumber287 {
 
     }
 
+    static int findDuplicateOptimal(int[] nums){
+        int n = nums.length;
+        if(n==0 || n==1) return -1;
+
+        for(int i=0; i<n; i++){
+            int num = Math.abs(nums[i]);
+            int idx = num-1;
+
+            if(nums[idx] <0) return num;
+            else{
+                nums[idx] *= -1;
+            }
+        }
+        return -1;
+    }
+
     public static void main(String[] args){
         int[] arr = {1,3,4,2,2};
 
-        int dupElement = findDuplicate(arr);
-
+        // Better Solution : HashMap
+        int dupElement = findDuplicateBetter(arr);
         System.out.println("Duplicate Element in given input array : "+dupElement);
+
+        // Optimal Solution : elements in range [1,n], mark visited elements to -ve
+        int dupElement1 = findDuplicateOptimal(arr);
+        System.out.println("Duplicate Element in given input array : "+dupElement1);
     }
 }
