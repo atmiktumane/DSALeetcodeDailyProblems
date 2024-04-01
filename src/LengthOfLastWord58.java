a1445d00b5799d48409eefa3c5c51c79f08528ec
atmiktumane <atmiktumane72@gmail.com>
2024-04-01 21:13:15 +0530

    added leetcode question 58

---
+++ src/LengthOfLastWord58.java
@@ -0,0 +1,69 @@
+import java.util.Stack;
+
+public class LengthOfLastWord58 {
+    static int lengthOfLastWordBetter(String s) {
+        int n = s.length();
+
+        if(n==0) return 0;
+
+        Stack<String> st = new Stack<>();
+        String temp = "";
+
+        for(int i=0; i<n; i++){
+            if(s.charAt(i)!=' '){
+                temp += s.charAt(i);
+            }
+            else{
+                if(temp.length()>0)
+                    st.push(temp);
+                temp = "";
+            }
+        }
+
+        if(temp!=""){
+            return temp.length();
+        }
+        else{
+            return st.peek().length();
+        }
+    }
+
+    static int lengthOfLastWordOptimal(String s) {
+        int n = s.length();
+
+        if(n==0) return 0;
+
+        String currStr = "";
+        String prevStr = "";
+
+        for(int i=0; i<n; i++){
+            if(s.charAt(i)!=' '){
+                currStr += s.charAt(i);
+            }
+            else{
+                if(currStr.length()>0)
+                    prevStr = currStr;
+                currStr = "";
+            }
+        }
+
+        if(currStr!=""){
+            return currStr.length();
+        }
+        else{
+            return prevStr.length();
+        }
+    }
+
+    public static void main(String[] args) {
+        String str = "luffy is still joyboy";
+
+        // Better Approach : Using stack
+        int res1 = lengthOfLastWordBetter(str);
+        System.out.println("Length of Last word : "+res1);
+
+        // Optimal Approach : Using 2 variables (currString, PrevString)
+        int res2 = lengthOfLastWordOptimal(str);
+        System.out.println("Length of Last word : "+res2);
+    }
+}
