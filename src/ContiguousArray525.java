b18a59481c34dfb061fec9d549c9f6fab6560418
atmiktumane <atmiktumane72@gmail.com>
2024-04-01 08:14:21 +0530

    added optimal solution for problem 525

--- src/ContiguousArray525.java
+++ src/ContiguousArray525.java
@@ -1,5 +1,7 @@
+import java.util.HashMap;
+
 public class ContiguousArray525 {
-    static void findContigousArrayBrute(int[] arr){
+    static void findMaxLengthBrute(int[] arr){
         int n=arr.length;
 
         int maxLen = Integer.MIN_VALUE;
@@ -19,9 +21,41 @@ public class ContiguousArray525 {
 
         System.out.println("Max. Length of contiguous array with equal no. of 0s and 1s : "+maxLen);
     }
+
+    static void findMaxLengthOptimal(int[] arr) {
+        int n = arr.length;
+
+        if(n==0 || n==1) return ;
+
+        HashMap<Integer, Integer> mp = new HashMap<>();
+        int currSum = 0, maxLen = 0;
+        mp.put(currSum, -1);
+
+        for(int i=0; i<n; i++){
+            if(arr[i]==0){   // treat 0 as -1
+                currSum += -1;
+            }
+            else{
+                currSum += arr[i];
+            }
+
+            if(!mp.containsKey(currSum)){
+                mp.put(currSum, i);
+            }
+            else{
+                int len = i-mp.get(currSum);
+                maxLen = Math.max(maxLen, len);
+            }
+        }
+
+        System.out.println("Max. Length of contiguous array with equal no. of 0s and 1s : "+maxLen);
+
+    }
     public static void main(String[] args){
         int[] arr = {0,1,0,0,0,1,1};
 
-        findContigousArrayBrute(arr);
+        findMaxLengthBrute(arr);
+
+        findMaxLengthOptimal(arr);
     }
 }
