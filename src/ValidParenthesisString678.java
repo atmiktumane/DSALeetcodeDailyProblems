50368360835fd38406c3383e6377b3a36cf256e6
atmiktumane <atmiktumane72@gmail.com>
2024-04-07 21:41:58 +0530

    updated better and optimal solution for leetcode 678

--- src/ValidParenthesisString678.java
+++ src/ValidParenthesisString678.java
@@ -2,8 +2,59 @@
 // TC : O(2N)
 // SC : O(1)
 
+import java.util.Stack;
+
 public class ValidParenthesisString678 {
-    static boolean checkValidString(String s) {
+
+    static boolean checkValidStringBetter(String s) {
+        int n = s.length();
+
+        Stack<Integer> openSt = new Stack<>();
+        Stack<Integer> asterikSt = new Stack<>();
+
+        for(int i=0; i<n; i++){
+            if(s.charAt(i) == '('){
+                openSt.push(i);
+            }
+            else if(s.charAt(i) == '*'){
+                asterikSt.push(i);
+            }
+            else{
+                if(!openSt.isEmpty()){
+                    openSt.pop();
+                }
+                else if(!asterikSt.isEmpty()){
+                    asterikSt.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+
+        if(openSt.isEmpty()){
+            return true;
+        }
+        else{
+            while(!openSt.isEmpty()){
+                if(asterikSt.isEmpty()){
+                    return false;
+                }
+                else{
+                    if(openSt.peek() < asterikSt.peek()){
+                        openSt.pop();
+                        asterikSt.pop();
+                    }
+                    else{
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+    static boolean checkValidStringOptimal(String s) {
         int n = s.length();
 
         int openCount = 0;
@@ -39,9 +90,20 @@ public class ValidParenthesisString678 {
     public static void main(String[] args){
         String s = "(*))";
 
-        boolean res = checkValidString(s);
+        // Better Solution -> Using 2 Stacks  -> TC: O(2N)  ;  SC: O(2N)
+        boolean res1 = checkValidStringBetter(s);
+        if(res1==true){
+            System.out.println("String is Valid");
+        }
+        else{
+            System.out.println("String is Not Valid");
+        }
 
-        if(res==true){
+
+        // Optimal Solution -> Using 2 variables (openCount, closeCount)  -> TC: O(2N)  ; SC: O(1)
+        boolean res2 = checkValidStringOptimal(s);
+
+        if(res2==true){
             System.out.println("String is Valid");
         }
         else{
