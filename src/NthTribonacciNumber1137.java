26113080a09cc32501cbe9ea13f10839d11c444d
atmiktumane <atmiktumane72@gmail.com>
2024-04-24 22:35:26 +0530

    modified leetcode question 1137

--- src/NthTribonacciNumber1137.java
+++ src/NthTribonacciNumber1137.java
@@ -16,13 +16,35 @@ public class NthTribonacciNumber1137 {
         return sum;
     }
 
+    static int findTribonacciBrute(int n){
+        if(n==0) return n;
+
+        if(n==1 || n==2) return 1;
+
+        int[] arr = new int[n+1];
+
+        arr[0] = 0;
+        arr[1] = 1;
+        arr[2] = 1;
+
+        for(int i=3; i<=n; i++){
+            arr[i] = arr[i-1] + arr[i-2] + arr[i-3];
+        }
+
+        return arr[n];
+    }
+
     public static void main(String[] args) {
         int n = 5;
 
-        // Optimal Approach  -> TC: O(1) , SC: O(1)
-        int res = findTribonacciOptimal(n);
+        // Brute Force -> TC: O(n) , SC: O(N)
+        int res1 = findTribonacciBrute(n);
+        System.out.println(res1);
 
-        System.out.println(res);
+        // Optimal Approach  -> TC: O(1) , SC: O(1)
+//        int res2 = findTribonacciOptimal(n);
+//
+//        System.out.println(res2);
 
 
     }
